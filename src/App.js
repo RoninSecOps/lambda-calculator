55f243e5c6910952bde0ae25d9fd8847e129ced6
Rafal Kaniewski <rafal.kaniewski@gmail.com>
2019-11-06 04:19:14 +0100

    Done mvp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Display from './components/DisplayComponents/Display';
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -12,12 +16,40 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [operation, setOperation] = useState(null);
+  const [currDisplay, setCurrDisplay] = useState(null);
 
+  const clearEverything = () => {
+    setNum1(0);
+    setNum2(0);
+    setOperation(null);
+    setCurrDisplay(null);
+  };
   return (
-    <div className='container'>
-      <Logo />
-      <div className='App'>
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+    <div>
+      <div className='container'>
+        <Logo />
+        <div id='app'>
+          <Display currDisplay={currDisplay} />
+          <div id='numpad'>
+            <Specials clearEverything={clearEverything} />
+            <Numbers
+              setNum1={setNum1}
+              setNum2={setNum2}
+              nums={{ num1: num1, num2: num2 }}
+              setCurrDisplay={setCurrDisplay}
+              operation={operation}
+            />
+          </div>
+          <Operators
+            operation={operation}
+            setOperation={setOperation}
+            setCurrDisplay={setCurrDisplay}
+            nums={{ num1: num1, num2: num2 }}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { numbers } from '../../../data';
 import NumberButton from './NumberButton';
 //import any components needed
@@ -8,17 +8,28 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = props => {
   // STEP 2 - add the imported data to state
+
   const [numberState, setNumberState] = useState(numbers);
+
   return (
-    <div>
+    <div id='numbers'>
       {numberState.map((number, index) => {
-        <NumberButton />;
+        return (
+          <NumberButton
+            key={index}
+            number={number}
+            setNum1={props.setNum1}
+            setNum2={props.setNum2}
+            nums={{ num1: props.nums.num1, num2: props.nums.num2 }}
+            setCurrDisplay={props.setCurrDisplay}
+            operation={props.operation}
+          />
+        );
       })}
-      /* STEP 3 - Use .map() to iterate over your array data and return a button
-      component matching the name on the provided file. Pass it any props needed
-      by the child component*/}
     </div>
   );
 };
+
+export default Numbers;
